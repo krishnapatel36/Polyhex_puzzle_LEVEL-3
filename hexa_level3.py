ba5873bb1b818ef8884b847fa6879d64dc4218ff
Krishna Patel <157887090+krishnapatel36@users.noreply.github.com>
2024-05-17 15:20:48 +0530

    Update hexa_level3.py

--- hexa_level3.py
+++ hexa_level3.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from pentominoes import ALL_PARTS
 from polymino import Grid,generate_polyminoes,generate_polymino_positions,unique_grids
 from dlx import DLX
 from hexa_rotation_flip import P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13
